fe517f6dd5933660a6a0eef6268cbcccd06f13e7
Laszlo Daru <daruu@hotmail.com>
2019-01-04 00:40:28 +0100

    Cocktail app basic setup

---
+++ Templating/Cocktails/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const controller = require('./controllers/controller');
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+controller(app);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  
+});
