5118e442dfd08be1effecb2f2edc490cfd9d8476
Laszlo Daru <daruu@hotmail.com>
2019-01-08 13:40:26 +0100

    searching for characters works

--- StarWars/assets/js/app.js
+++ StarWars/assets/js/app.js
@@ -1,16 +1,36 @@
+const characterDiv =  document.getElementsByClassName('character')[0];
 const searchButton = document.querySelector('#search');
 searchButton.addEventListener('click', () => {
     const myReq = new XMLHttpRequest();
     const getName = document.querySelector('#name');
-    // console.log(getName.value);
 
     myReq.open('GET', 'https://swapi.co/api/people/?search=' + getName.value, true);
 
     myReq.onload = () => {
         if(myReq.status >= 200 && myReq.status < 400){
             let myData = JSON.parse(myReq.responseText);
-            console.log(myData);
+            // renderHTML(myData);
+            
+            for( let i = 0; i < myData.results.length; i++){
+                let characterNames = document.createElement('p');
+                console.log(myData.results[i].name);
+                characterNames.textContent = myData.results[i].name;
+
+                characterDiv.appendChild(characterNames);
+            
+            }
+            
         }
     }
     myReq.send();
 });
+
+// const renderHTML = (data) => {
+//     htmlString ="";
+
+//     for(let i = 0; i < data.results.length; i++){
+//         htmlString += '<p>' + data.results[i].name + '</p>';
+        
+//     }
+//     characterDiv.insertAdjacentHTML('beforeend', htmlString);
+// };
