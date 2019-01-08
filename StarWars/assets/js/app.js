40baff76b1f85d88929c3fb1fe55349cd3628fc2
Laszlo Daru <daruu@hotmail.com>
2019-01-08 19:59:22 +0100

    Rendering page successfully, no css

--- StarWars/assets/js/app.js
+++ StarWars/assets/js/app.js
@@ -1,24 +1,18 @@
+window.onload = () => {
 const characterDiv =  document.getElementsByClassName('character')[0];
+const movieDiv = document.getElementsByClassName('movies')[0];
 const searchButton = document.querySelector('#search');
 searchButton.addEventListener('click', () => {
     const myReq = new XMLHttpRequest();
     const getName = document.querySelector('#name');
 
     myReq.open('GET', 'https://swapi.co/api/people/?search=' + getName.value, true);
-
     myReq.onload = () => {
         if(myReq.status >= 200 && myReq.status < 400){
             let myData = JSON.parse(myReq.responseText);
             renderHTML(myData);
             
-            // for( let i = 0; i < myData.results.length; i++){
-            //     let characterNames = document.createElement('p');
-            //     console.log(myData.results[i].name);
-            //     characterNames.textContent = myData.results[i].name;
-
-            //     characterDiv.appendChild(characterNames);
-            
-            // }
+            getMovies(myData);
             
         }
     }
@@ -27,8 +21,35 @@ searchButton.addEventListener('click', () => {
 
 const renderHTML = (data) => {
     for(let i = 0; i < data.results.length; i++){
-        let characterNames = document.createElement('p');
+        let characterNames = document.createElement('h5');
         characterNames.textContent = data.results[i].name;
         characterDiv.appendChild(characterNames);   
     }
 };
+    const getMovies = (data) => {
+            const h5Tags = document.getElementsByTagName('h5');
+            
+            for(let i = 0; i < h5Tags.length; i++){
+                h5Tags[i].onclick = () => {
+                    for( let j = 0; j < data.results[i].films.length; j++){
+                        let urlHost = data.results[i].films[j];
+                        let http = new XMLHttpRequest();
+
+                        http.open('GET', urlHost, true);
+                        http.onload = () => {
+                            let movie = JSON.parse(http.response);
+                            let movieTitle = document.createElement('li');
+                            movieTitle.textContent = movie.title;
+                            movieDiv.appendChild(movieTitle);
+                            let releaseDate = document.createElement('p');
+                             releaseDate.textContent = 'Release Date: ' + movie.release_date;
+                             movieTitle.appendChild(releaseDate);
+                        }
+                    
+                        http.send();
+                    }
+                }
+               
+            }
+    }
+}
