dd7b48c6263b9c952338e3162679c2bf5473ed38
Laszlo Daru <daruu@hotmail.com>
2019-01-09 20:52:43 +0100

    basics CSS for Star Wars search

--- StarWars/assets/js/app.js
+++ StarWars/assets/js/app.js
@@ -4,25 +4,32 @@ const searchButton = document.querySelector('#search');
 searchButton.addEventListener('click', () => {
     const myReq = new XMLHttpRequest();
     const getName = document.querySelector('#name');
-
+    if(getName.value === "" || getName.value.length < 2){
+        alert('The search box is empty, or it`s  less than 2 characters!');
+    }else{
     myReq.open('GET', 'https://swapi.co/api/people/?search=' + getName.value, true);
     myReq.onload = () => {
         if(myReq.status >= 200 && myReq.status < 400){
             let myData = JSON.parse(myReq.responseText);
             renderHTML(myData);
-            
             getMovies(myData);
             
         }
+    }
     }
     myReq.send();
 });
 
 const renderHTML = (data) => {
-    for(let i = 0; i < data.results.length; i++){
-        let characterNames = document.createElement('h4');
-        characterNames.textContent = data.results[i].name;
-        characterDiv.appendChild(characterNames);   
+    if(data.results.length > 0){
+        let discription = document.createElement('h3');
+        discription.textContent = 'Click on the name to list the character movies!';
+        characterDiv.appendChild(discription);
+     for(let i = 0; i < data.results.length; i++){
+          let characterNames = document.createElement('h4');
+          characterNames.textContent = data.results[i].name;
+          characterDiv.appendChild(characterNames);   
+        }
     }
 };
     const getMovies = (data) => {
@@ -32,14 +39,15 @@ const renderHTML = (data) => {
 
                     if(data.results[i].films.length > 0){
                         let movieHead = document.createElement('h4');
-                        movieHead.textContent = 'Movie Appearance: ';
+                        movieHead.textContent = data.results[i].name +' Appearance in movies: ';
                         movieDiv.appendChild(movieHead);
                         let moviesList = document.createElement('ol');
                         movieHead.appendChild(moviesList);
 
                     for( let j = 0; j < data.results[i].films.length; j++){
                         let urlHost = data.results[i].films[j];
-                        let http = new XMLHttpRequest();
+                        const http = new XMLHttpRequest();
+
                         http.open('GET', urlHost, true);
                         http.onload = () => {
 
@@ -48,7 +56,7 @@ const renderHTML = (data) => {
                             movieTitle.textContent = movie.title;
                             movieDiv.appendChild(movieTitle);
                             let releaseDate = document.createElement('span');
-                             releaseDate.textContent = 'Release Date: ' + movie.release_date;
+                             releaseDate.textContent = '  Release Date: ' + movie.release_date;
                              movieTitle.appendChild(releaseDate);
                         }
                     
