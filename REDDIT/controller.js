bc7e94187f573664495089c97944d9e06f5e8486
Laszlo Daru <daruu@hotmail.com>
2019-01-15 22:43:50 +0100

    feat(post endpoint)

--- REDDIT/controller.js
+++ REDDIT/controller.js
@@ -1,5 +1,6 @@
 module.exports = (app) =>{
 
+    const dateTime = require('./datetime');
     const mysql = require('mysql');
     const connection = mysql.createConnection({
         host     : 'localhost',
@@ -31,4 +32,30 @@ module.exports = (app) =>{
             }
         });
       });
+   
+   app.post('api/posts', (req, res) => {
+       const title = req.body.title;
+       const url =  req.body.url;
+       const vote = 0;
+       const date = dateTime();
+       if(title == undefined || url == undefined){
+           res.status(418).send('Title or URL is missing!');
+       }else{
+           connection.query(`INSERT INTO posts(title, url, vote, date) VALUES(?, ?, ?, ?);`,[title, url, vote, date], function(err, result){
+               if(err){
+                console.log(err.message);
+                res.status(500).send('Error with Database!');
+                return;
+               }
+               connection.query(`SELECT * FROM posts WHERE title = ? AND url = ? AND vote = ? AND date = ?;`, [title, url, vote, date], function(err,result){
+                   if(err){
+                    console.log(err.message);
+                    res.status(500).send('Error with Database!');
+                    return;
+                   }
+                   res.send(result);
+               });
+           });
+       }
+   });
 }
