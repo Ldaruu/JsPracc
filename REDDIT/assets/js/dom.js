1bffb50115da4bc10096e596ccd82b69392043f2
Laszlo Daru <daruu@hotmail.com>
2019-01-16 17:01:40 +0100

    feat(frontedn):ajax & DOM

---
+++ REDDIT/assets/js/dom.js
@@ -0,0 +1,26 @@
+const list = document.getElementById('msg');
+const host = 'http://localhost:8080/api/posts';
+
+const http =  new XMLHttpRequest();
+http.open('GET', host, true);
+http.onload = () => {
+    if(http.status >= 200 && http.status < 400){
+        let result = JSON.parse(http.responseText);
+        renderHTML(result);
+    }
+}
+http.send();
+
+const renderHTML = (data) => {
+   let HTMLString = "";
+   for( let i =0; i < data.length; i++){
+       HTMLString += '<div class="post">';
+       HTMLString += '<a class="upvote" id="' + data[i].id +'" href="#">⬆</a>';
+       HTMLString += '<span class="vote">'+ data[i].vote +'</span>';
+       HTMLString += '<a class="downvote" id="' + data[i].id +'" href="#">⬇</a>';
+       HTMLString += '<a class="url" href='+data[i].url+'><span class="title" ></span>'+ data[i].title +'</a>'
+       HTMLString += '<span class="date">'+ data[i].date +'</span>';
+       HTMLString += '</div>';             
+   }
+   list.insertAdjacentHTML('beforeend',HTMLString);
+} 
