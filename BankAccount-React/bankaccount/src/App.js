580089a925d189cac52cbba9beaecf0255e9d63c
Laszlo Daru <daruu@hotmail.com>
2019-01-22 14:43:02 +0100

    feat(Link to):added to Navbar to prevent page reload

--- BankAccount-React/bankaccount/src/App.js
+++ BankAccount-React/bankaccount/src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
-       
       </div>
       </BrowserRouter>
     );
