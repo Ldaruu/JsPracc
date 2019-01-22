c066c5516aee3877def1c29077c8bfaf5b7d16ff
Laszlo Daru <daruu@hotmail.com>
2019-01-22 17:20:15 +0100

    feat(axios API request)

--- BankAccount-React/bankaccount/src/Home.js
+++ BankAccount-React/bankaccount/src/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Accounts from './Accounts';
 import AddAccount from './AddAccount';
+import axios from 'axios';
 
 class Home extends Component{
     state = {
@@ -9,8 +10,19 @@ class Home extends Component{
           {name: 'John', age: 33, city:'Budapest',  balance: 15000, id: 2},
           {name: 'Linda', age: 28, city:'NewYork', balance: 20000, id: 3},
           {name: 'Susan', age: 45, city:'Toronto',balance: 330000, id: 4}
-        ]
+        ],
+        img: []
       }
+      componentDidMount(){
+        axios.get('http://api.giphy.com/v1/gifs/search?q=money&api_key=LCTqQxbLKacDnf61FqHGzFLTeW6qZPI8')
+            .then(res => {
+                console.log(res.data.data);
+                this.setState({
+                    img: res.data.data.slice(1,2)
+                })
+            })
+    }
+
       addAccount = (account) => {
         account.id = Math.floor(Math.random() * Math.floor(100));
         let newAccount = [...this.state.accounts, account];
@@ -28,10 +40,25 @@ class Home extends Component{
       }
       
       render() {
+          const{img} = this.state
+          const images = img.length ? (
+              img.map(pic => {
+                  let imgUrl = pic.images.original.url ;
+                  console.log(imgUrl);
+                  return(
+                      <div key={pic.id}>
+                        <img src={imgUrl}  alt="$$$" height="200" width="200"/>
+                      </div>
+                  )
+              })
+          ) : (
+              <div>Bank is Closed!</div>
+          )
         return (
           <div className="App">
            <h1>My first React App!</h1>
            <h4>Bank of React</h4>
+           {images}
            <Accounts deleteAccout={this.deleteAccout} accounts={this.state.accounts}/>
            <AddAccount addAccount ={this.addAccount} />
           </div>
